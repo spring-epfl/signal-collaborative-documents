a8bfa310cf67396553618767ece01c97a6e27179
Christian Knabenhans <christian.knabenhans@gmail.com>
2026-03-05 18:21:37 +0800

    Specify bash for scripts, Use docker compose instead of docker-compose

--- experiment-2.sh
+++ experiment-2.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/usr/bin/env bash
 set -euo pipefail
 
 # Load env vars if present
 [ -f .env ] && . ./.env
 
-docker-compose up --build -d
+docker compose up --build -d
 
 # Run benchmarks for both modes
 ./netprofile.sh slow
@@ -18,7 +18,7 @@ mv benchmark_data/s2-signal.csv benchmark_data/s2-signal-fast.csv
 docker exec -it e2ee-cd /bin/bash ./b3.sh
 mv benchmark_data/s3-signal.csv benchmark_data/s3-signal-fast.csv
 
-docker-compose down
+docker compose down
 
 # Generate plots
 uv run --with jupyter jupyter execute analysis-2-3.ipynb
